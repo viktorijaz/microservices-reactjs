21df7fae0c97512ae666a012610446b27896adba
viktorija <viktorija11@gmail.com>
2020-08-17 16:05:43 +0200

    tickets api

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { createTicketRouter } from './routes/new';
 import { errorHandler, NotFoundError, currentUser } from '@vikitickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,7 +18,11 @@ app.use(
   })
 );
 app.use(currentUser);
+
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
