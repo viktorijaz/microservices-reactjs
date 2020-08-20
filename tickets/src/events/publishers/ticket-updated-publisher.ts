f97ed4632312a4d924f96055aa6d44e5050a4dba
viktorija <viktorija11@gmail.com>
2020-08-20 14:59:30 +0200

    tickets add publish / listener for messages

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@vikitickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
